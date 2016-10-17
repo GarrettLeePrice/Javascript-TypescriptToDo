7e26f19c8ac88f5bd2e610e09d1f3be71f66ea7b
garrett price and stephen newkirk <garrett@>
2016-10-17 10:47:53 -0700

    Complete basic typescript to-do. No known bugs currently.

---
+++ app/to-do-create-tasks.ts
@@ -0,0 +1,31 @@
+/// <reference path="to-do-classes-interfaces.ts" />
+/// <reference path="to-do-people.ts" />
+/// <reference path="to-do-listing-functions.ts" />
+
+var people = ToDoList.people;
+
+var tasks = [];
+tasks.push(new ToDoList.HomeTask("Do the dishes.", "High"));
+tasks.push(new ToDoList.HomeTask("Buy chocolate.", "Low"));
+tasks.push(new ToDoList.HomeTask("Wash the laundry.", "High"));
+tasks[0].markDone();
+
+tasks.push(new ToDoList.HobbyTask("Practice origami."));
+tasks.push(new ToDoList.HobbyTask("Bake a pie."));
+
+var today = new Date();
+var tomorrow = today;
+
+tomorrow.setDate(today.getDate() + 1);
+var nextDay = today;  // create another date object and set it to today's date plus 2.
+nextDay.setDate(today.getDate() + 2);
+
+tasks.push(new ToDoList.WorkTask(today, "Update blog.", "High", people.thor));
+tasks.push(new ToDoList.WorkTask(tomorrow, "Go to meeting.", "Medium", people.thor));
+tasks.push(new ToDoList.WorkTask(nextDay, "Clean ceiling.", "Low", people.thor));
+
+var thorTasks = ToDoList.describeTasksForPerson(people.thor, tasks);
+console.log("Here are Thor's tasks: ");
+for(var task of thorTasks){
+  console.log(task);
+}
